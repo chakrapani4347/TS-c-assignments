6abafc07a80af763b209a9e803d02ea283a152fd
chakrapani4347 <104552194+chakrapani4347@users.noreply.github.com>
2022-05-18 17:10:55 +0530

    Create 6.c

---
+++ patterns/6.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+int i,j,rows,space;
+printf("enter no. of rows:");
+scanf("%d",&rows);
+ for(i=rows;i>=1;i--)
+  {
+    for(space=1;space<=(rows-i);space++)
+   {
+      printf(" ");
+   }
+    for(j=1;j<=2*i-1;j++)
+     {
+       printf("*");
+     }
+  printf("\n");
+   }
+for(i=2;i<=rows;i++)
+  {
+    for(space=1;space<=(rows-i);space++)
+   {
+      printf(" ");
+   }
+for(j=1;j<=2*i-1;j++)
+  {
+    printf("*");
+  }
+printf("\n");
+}
+return 0;
+}
+
+
+/*output
+enter no. of rows:6
+***********
+ *********
+  *******
+   *****
+    ***
+     *
+    ***
+   *****
+  *******
+ *********
+***********
+*/
