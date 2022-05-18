9bae6927d220dc9f9bef6c2e1ebdda88f8c8e74d
chakrapani4347 <104552194+chakrapani4347@users.noreply.github.com>
2022-05-18 17:34:33 +0530

    Create 8.c

---
+++ patterns/set_1/8.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+int main()
+{
+        int i,j,no;
+        for(i=1;i<=5;i++)
+        {
+                for(j=1;j<=i;j++)
+                {
+                        no++;
+                printf("%d ",no);
+                }
+                printf("\n");
+        }
+return 0;
+}
+
+
+
+
+/*
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
+*/
