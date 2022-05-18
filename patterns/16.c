62ec19a2a6b0c9cef4c87edf94c75e86d5e079a2
chakrapani4347 <104552194+chakrapani4347@users.noreply.github.com>
2022-05-18 17:19:10 +0530

    Create 16.c

---
+++ patterns/16.c
@@ -0,0 +1,56 @@
+
+
+
+#include<stdio.h>
+int main()
+{
+int i,j,k,n=5,m=0,p;
+printf("The pattern is:\n");
+for(i=1;i<=n;i++)
+{
+	for(k=1;k<=n-i;k++)
+        {
+	        printf("   ");
+        }
+        for(p=m,j=1;j<=(i*2)-1;j++,p++)
+        {               
+                printf("%d",p);
+                printf(" ");       
+        }
+        m--;
+        printf("\n");
+}
+
+i=i-2;
+m++;
+for(i=i;i>=1;i--)
+{
+	for(k=n-i;k>=1;k--)
+        {
+        	printf("   ");
+        }
+        m++;
+        for(p=m,j=(i*2)-1;j>=1;j--,p++)
+        {               
+        	printf("%d",p);
+                printf(" ");
+
+        }
+        printf("\n");        
+}
+return 0;
+}
+
+
+/*           
+ The pattern is:
+            0 
+         -1 0 1 
+      -2 -1 0 1 2 
+   -3 -2 -1 0 1 2 3 
+-4 -3 -2 -1 0 1 2 3 4 
+   -3 -2 -1 0 1 2 3 
+      -2 -1 0 1 2 
+         -1 0 1 
+            0 
+        */
