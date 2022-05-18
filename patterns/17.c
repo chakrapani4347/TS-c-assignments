26fdd5a7fe51e713bf84afe2d5e397887c52e987
chakrapani4347 <104552194+chakrapani4347@users.noreply.github.com>
2022-05-18 17:20:03 +0530

    Create 17.c

---
+++ patterns/17.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+int i,j,n=5;
+for(i=1;i<n;i++)
+{
+ for(j=1;j<=i;j++)
+  printf("%d",j);
+  printf("\n");
+}
+for(i=n;i>=0;i--)
+ {
+for(j=1;j<=i;j++)
+printf("%d",j);
+printf("\n");
+ }
+return 0;
+}
+
+
+/*output
+1
+12
+123
+1234
+12345
+1234
+123
+12
+1
+*/
